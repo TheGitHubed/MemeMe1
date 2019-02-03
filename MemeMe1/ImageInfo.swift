48501c5b96fb49b35a3e3f268e66beade4136f02
TheGitHubed <41701237+TheGitHubed@users.noreply.github.com>
2019-02-03 14:53:27 +0300

    Add files via upload

---
+++ MemeMe1/ImageInfo.swift
@@ -0,0 +1,19 @@
+//
+//  ImageInfo.swift
+//  MemeMe1
+//
+//  Created by Ahmed on 13/11/2018.
+//  Copyright © 2018 Ahmed. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Meme
+{
+   var topText: String
+   var  bottomText: String
+  var originalImage: UIImage
+  var memedImage: UIImage
+    
+}
